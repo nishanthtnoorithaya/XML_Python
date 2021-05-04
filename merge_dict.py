2c7d53c6e0935b4f94570fdfa66913a5defeb108
nishanthtnoorithaya <nishanth.t40@gmail.com>
2021-05-04 15:34:06 +0530

    Json File
    This is used in main Creating_json_file.py file to merge the 2 dictionaries

---
+++ merge_dict.py
@@ -0,0 +1,8 @@
+def mergeDict(dict1, dict2):
+   ''' Merge dictionaries and keep values of common keys in list'''
+   dict3 = {**dict1, **dict2}
+   for key, value in dict3.items():
+       if key in dict1 and key in dict2:
+               dict3[key] = [value , dict1[key]]
+ 
+   return dict3
